4773a9bd885c43740d5555954c24a51433cb9c43
Rujulsalunhke <salunkhe.rujul@gmail.com>
2024-10-03 00:09:40 +0530

    don't pull

--- asset-transfer-basic/chaincode-javascript/lib/assetTransfer.js
+++ asset-transfer-basic/chaincode-javascript/lib/assetTransfer.js
@@ -7,8 +7,8 @@
 'use strict';
 
 // Deterministic JSON.stringify()
-const stringify  = require('json-stringify-deterministic');
-const sortKeysRecursive  = require('sort-keys-recursive');
+const stringify = require('json-stringify-deterministic');
+const sortKeysRecursive = require('sort-keys-recursive');
 const { Contract } = require('fabric-contract-api');
 
 class AssetTransfer extends Contract {
@@ -20,57 +20,52 @@ class AssetTransfer extends Contract {
                 Color: 'blue',
                 Size: 5,
                 Owner: 'Tomoko',
-                AppraisedValue: 300,
+                DocumentURL: 'QmV4ViggPP4kBBSBPhF2CMUyBsGLr8mJ33Txfb9Wj9WGuZ' // Add document URL or IPFS hash field
             },
             {
                 ID: 'asset2',
                 Color: 'red',
                 Size: 5,
                 Owner: 'Brad',
-                AppraisedValue: 400,
+                DocumentURL: 'Qmeyy8DjQMtjNfNngG3woDmjRXwhZhvrcfKHWt8Hf9cwbR'
             },
             {
                 ID: 'asset3',
                 Color: 'green',
                 Size: 10,
                 Owner: 'Jin Soo',
-                AppraisedValue: 500,
+                DocumentURL: 'QmcpQGsvPhGVVGfEKPi43aN1dbFf8y91MoLdzQaSuNjo94'
             },
             {
                 ID: 'asset4',
                 Color: 'yellow',
                 Size: 10,
                 Owner: 'Max',
-                AppraisedValue: 600,
+                DocumentURL: 'QmdBZWVcSbcRtJZCdY59U7cRVh6xCu2Y66ZDQid3JWXyKW'
             },
             {
                 ID: 'asset5',
                 Color: 'black',
                 Size: 15,
                 Owner: 'Adriana',
-                AppraisedValue: 700,
+                DocumentURL: 'QmUiRWB3e1ob3MaPD51JMjBALVfEZG1eaVicnJA3PHenU1'
             },
             {
                 ID: 'asset6',
                 Color: 'white',
                 Size: 15,
                 Owner: 'Michel',
-                AppraisedValue: 800,
+                DocumentURL: 'QmdSLsY7RQo8mRk5NNMBSxATNQDFLTu8c37Kdn6GUiKSxu'
             },
         ];
 
         for (const asset of assets) {
             asset.docType = 'asset';
-            // example of how to write to world state deterministically
-            // use convetion of alphabetic order
-            // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
-            // when retrieving data, in any lang, the order of data will be the same and consequently also the corresonding hash
             await ctx.stub.putState(asset.ID, Buffer.from(stringify(sortKeysRecursive(asset))));
         }
     }
 
-    // CreateAsset issues a new asset to the world state with given details.
-    async CreateAsset(ctx, id, color, size, owner, appraisedValue) {
+    async CreateAsset(ctx, id, color, size, owner, documentURL) {
         const exists = await this.AssetExists(ctx, id);
         if (exists) {
             throw new Error(`The asset ${id} already exists`);
@@ -81,42 +76,36 @@ class AssetTransfer extends Contract {
             Color: color,
             Size: size,
             Owner: owner,
-            AppraisedValue: appraisedValue,
+            DocumentURL: documentURL // Store document URL or IPFS hash
         };
-        // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
         await ctx.stub.putState(id, Buffer.from(stringify(sortKeysRecursive(asset))));
         return JSON.stringify(asset);
     }
 
-    // ReadAsset returns the asset stored in the world state with given id.
     async ReadAsset(ctx, id) {
-        const assetJSON = await ctx.stub.getState(id); // get the asset from chaincode state
+        const assetJSON = await ctx.stub.getState(id);
         if (!assetJSON || assetJSON.length === 0) {
             throw new Error(`The asset ${id} does not exist`);
         }
         return assetJSON.toString();
     }
 
-    // UpdateAsset updates an existing asset in the world state with provided parameters.
-    async UpdateAsset(ctx, id, color, size, owner, appraisedValue) {
+    async UpdateAsset(ctx, id, color, size, owner, documentURL) {
         const exists = await this.AssetExists(ctx, id);
         if (!exists) {
             throw new Error(`The asset ${id} does not exist`);
         }
 
-        // overwriting original asset with new asset
         const updatedAsset = {
             ID: id,
             Color: color,
             Size: size,
             Owner: owner,
-            AppraisedValue: appraisedValue,
+            DocumentURL: documentURL // Update document URL or IPFS hash
         };
-        // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
-        return ctx.stub.putState(id, Buffer.from(stringify(sortKeysRecursive(updatedAsset))));
+        await ctx.stub.putState(id, Buffer.from(stringify(sortKeysRecursive(updatedAsset))));
     }
 
-    // DeleteAsset deletes an given asset from the world state.
     async DeleteAsset(ctx, id) {
         const exists = await this.AssetExists(ctx, id);
         if (!exists) {
@@ -125,27 +114,22 @@ class AssetTransfer extends Contract {
         return ctx.stub.deleteState(id);
     }
 
-    // AssetExists returns true when asset with given ID exists in world state.
     async AssetExists(ctx, id) {
         const assetJSON = await ctx.stub.getState(id);
         return assetJSON && assetJSON.length > 0;
     }
 
-    // TransferAsset updates the owner field of asset with given id in the world state.
     async TransferAsset(ctx, id, newOwner) {
         const assetString = await this.ReadAsset(ctx, id);
         const asset = JSON.parse(assetString);
         const oldOwner = asset.Owner;
         asset.Owner = newOwner;
-        // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
         await ctx.stub.putState(id, Buffer.from(stringify(sortKeysRecursive(asset))));
         return oldOwner;
     }
 
-    // GetAllAssets returns all assets found in the world state.
     async GetAllAssets(ctx) {
         const allResults = [];
-        // range query with empty string for startKey and endKey does an open-ended query of all assets in the chaincode namespace.
         const iterator = await ctx.stub.getStateByRange('', '');
         let result = await iterator.next();
         while (!result.done) {
